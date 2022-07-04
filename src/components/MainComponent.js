d59ab74caafe44d278a7f55396167487eac139d4
Vitalii Chaikovskyi <garber1329@gmail.com>
2022-07-04 15:18:32 +0300

    Assignment 2: React Router and Single Page Applications

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -35,6 +36,12 @@ class Main extends Component {
       );
     }
 
+    const AboutUs = () => {
+      return(
+          <About leaders={this.state.leaders} />
+      );
+    }
+
     const DishWithId = ({match}) => {
       return(
           <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
@@ -50,6 +57,7 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
+              <Route exact path='/aboutus' component={AboutUs} />
               <Redirect to="/home" />
           </Switch>
           <Footer />
